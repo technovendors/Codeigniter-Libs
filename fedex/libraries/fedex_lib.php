d1c04248ffdb08d78e24e7fe50341be6580333aa
Nitin Goyal <nitingoyal.dev@gmail.com>
2017-01-16 14:40:39 +0530

    Update and rename fedex.php to fedex_lib.php

---
+++ fedex/libraries/fedex_lib.php
@@ -0,0 +1,29 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.1.6 or newer
+ *
+ * @package     CodeIgniter
+ * @author      Nitin Goyal
+ * @copyright   Copyright (c) 2016, Crazywapbox Inc.
+ * @license     
+ * @link        http://www.crazywapbox.com
+ * @since       Version 1.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Crazywapbox core CodeIgniter class
+ *
+ * @package     CodeIgniter
+ * @subpackage          Libraries
+ * @category            Crazywapbox Inc. 
+ * @author      Nitin Goyal
+ * @link        http://www.crazywapbox.com
+ */
+ 
+class FedEx_Lib{
+}
